6787ad82c5f6d42144cf75cc4688f5a34bb1872d
djouhaina-chelbi <djuhaynach2023@gmail.com>
2025-11-05 10:47:02 +0100

    cod_07

--- Exam_points.py
+++ Exam_points.py
@@ -1,7 +1,3 @@
 def exam_points(point->int ,return->string):
-    if len(point) <20 :
-        errors.append("Very Excellent")
-      return {
-         "is_valid": len(point) == 0,
-         "errors": nep point
-      }
+    total = point,retun
+    return exam_point
--- test_Exam_points.py
+++ test_Exam_points.py
@@ -1,6 +1,11 @@
-def test_exam_points(point->int ,return->string):
-    result = exam_point("18")
-    assert result["is_valid"] == Very Excellent
+@ pytest.mark.params([('0',nep point),
+                      ('10',Unsuccessful),
+                      ('12',Good),
+                      ('14',Very good),
+                      ('16',Excellent),
+                      ('20',Very Excellent)])
 
-
+def test_exam_total_points(point->int ,return->string,expected_total):
+    actnal_total = exam_point( point,expected_total)
+    assert actnal_total == expected_total
 
