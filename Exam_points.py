ea7c1430e1003d4dd187ed39d53fb738edd2e609
djouhaina-chelbi <djuhaynach2023@gmail.com>
2025-11-05 10:22:23 +0100

    cod_05

--- Exam_points.py
+++ Exam_points.py
@@ -1,7 +1,7 @@
 def exam_points(point->int ,return->string):
-    if len(point) 14< point <12 :
-        errors.append("Very good")
+    if len(point) 16< point <14 :
+        errors.append("Excellent")
     return {
          "is_valid": len(point) == 0,
-          "errors": nep point
+         "errors": nep point
     }
--- test_Exam_points.py
+++ test_Exam_points.py
@@ -1,6 +1,6 @@
 def test_exam_points(point->int ,return->string):
-    result = exam_point("13")
-    assert result["is_valid"] == Very good
+    result = exam_point("15")
+    assert result["is_valid"] == Excellent
 
 
 
